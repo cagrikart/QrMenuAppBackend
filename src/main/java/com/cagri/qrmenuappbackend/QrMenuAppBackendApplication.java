1ca7e5bd1521d5574099a18362d456831ad19cdf
Çağrı KART <ckarte43@gmail.com>
2023-04-20 21:34:28 +0300

    first

---
+++ src/main/java/com/cagri/qrmenuappbackend/QrMenuAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cagri.qrmenuappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrMenuAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrMenuAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cagri/qrmenuappbackend/QrMenuAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagri.qrmenuappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrMenuAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
